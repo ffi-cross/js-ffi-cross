a4f3a7a57af0be3becafdb39e737f23d7edae6f8
Yonggang Luo <luoyonggang@gmail.com>
2021-05-03 02:22:23 +0800

    Add ref/buffer.js and TSD file

---
+++ types/lib/ref-buffer.ts
@@ -0,0 +1,73 @@
+
+export declare function readUInt8(buf: Buffer, offset: number, noAssert?: boolean): number;
+
+export declare function readUInt16LE(buf: Buffer, offset: number, noAssert?: boolean): number;
+export declare function readUInt16BE(buf: Buffer, offset: number, noAssert?: boolean): number;
+export declare function readUInt16(buf: Buffer, offset: number, noAssert?: boolean): number;
+
+export declare function readUInt32LE(buf: Buffer, offset: number, noAssert?: boolean): number;
+export declare function readUInt32BE(buf: Buffer, offset: number, noAssert?: boolean): number;
+export declare function readUInt32(buf: Buffer, offset: number, noAssert?: boolean): number;
+
+export declare function readBigUInt64LE(buf: Buffer, offset: number, noAssert?: boolean): bigint;
+export declare function readBigUInt64BE(buf: Buffer, offset: number, noAssert?: boolean): bigint;
+export declare function readBigUInt64(buf: Buffer, offset: number, noAssert?: boolean): bigint;
+
+export declare function readInt8(buf: Buffer, offset: number, noAssert?: boolean): number;
+
+export declare function readInt16LE(buf: Buffer, offset: number, noAssert?: boolean): number;
+export declare function readInt16BE(buf: Buffer, offset: number, noAssert?: boolean): number;
+export declare function readInt16(buf: Buffer, offset: number, noAssert?: boolean): number;
+
+export declare function readInt32LE(buf: Buffer, offset: number, noAssert?: boolean): number;
+export declare function readInt32BE(buf: Buffer, offset: number, noAssert?: boolean): number;
+export declare function readInt32(buf: Buffer, offset: number, noAssert?: boolean): number;
+
+export declare function readBigInt64LE(buf: Buffer, offset: number, noAssert?: boolean): bigint;
+export declare function readBigInt64BE(buf: Buffer, offset: number, noAssert?: boolean): bigint;
+export declare function readBigInt64(buf: Buffer, offset: number, noAssert?: boolean): bigint;
+
+export declare function readFloatLE(buf: Buffer, offset: number, noAssert?: boolean): number;
+export declare function readFloatBE(buf: Buffer, offset: number, noAssert?: boolean): number;
+export declare function readFloat(buf: Buffer, offset: number, noAssert?: boolean): number;
+
+export declare function readDoubleLE(buf: Buffer, offset: number, noAssert?: boolean): number;
+export declare function readDoubleBE(buf: Buffer, offset: number, noAssert?: boolean): number;
+export declare function readDouble(buf: Buffer, offset: number, noAssert?: boolean): number;
+
+
+export declare function writeUInt8(buf: Buffer, value: number, offset: number, noAssert?: boolean): number;
+
+export declare function writeUInt16LE(buf: Buffer, value: number, offset: number, noAssert?: boolean): number;
+export declare function writeUInt16BE(buf: Buffer, value: number, offset: number, noAssert?: boolean): number;
+export declare function writeUInt16(buf: Buffer, value: number, offset: number, noAssert?: boolean): number;
+
+export declare function writeUInt32LE(buf: Buffer, value: number, offset: number, noAssert?: boolean): number;
+export declare function writeUInt32BE(buf: Buffer, value: number, offset: number, noAssert?: boolean): number;
+export declare function writeUInt32(buf: Buffer, value: number, offset: number, noAssert?: boolean): number;
+
+export declare function writeBigUInt64LE(buf: Buffer, value: bigint, offset: number, noAssert?: boolean): number;
+export declare function writeBigUInt64BE(buf: Buffer, value: bigint, offset: number, noAssert?: boolean): number;
+export declare function writeBigUInt64(buf: Buffer, value: bigint, offset: number, noAssert?: boolean): number;
+
+export declare function writeInt8(buf: Buffer, value: number, offset: number, noAssert?: boolean): number;
+
+export declare function writeInt16LE(buf: Buffer, value: number, offset: number, noAssert?: boolean): number;
+export declare function writeInt16BE(buf: Buffer, value: number, offset: number, noAssert?: boolean): number;
+export declare function writeInt16(buf: Buffer, value: number, offset: number, noAssert?: boolean): number;
+
+export declare function writeInt32LE(buf: Buffer, value: number, offset: number, noAssert?: boolean): number;
+export declare function writeInt32BE(buf: Buffer, value: number, offset: number, noAssert?: boolean): number;
+export declare function writeInt32(buf: Buffer, value: number, offset: number, noAssert?: boolean): number;
+
+export declare function writeBigInt64LE(buf: Buffer, value: bigint, offset: number, noAssert?: boolean): number;
+export declare function writeBigInt64BE(buf: Buffer, value: bigint, offset: number, noAssert?: boolean): number;
+export declare function writeBigInt64(buf: Buffer, value: bigint, offset: number, noAssert?: boolean): number;
+
+export declare function writeFloatLE(buf: Buffer, value: number, offset: number, noAssert?: boolean): number;
+export declare function writeFloatBE(buf: Buffer, value: number, offset: number, noAssert?: boolean): number;
+export declare function writeFloat(buf: Buffer, value: number, offset: number, noAssert?: boolean): number;
+
+export declare function writeDoubleLE(buf: Buffer, value: number, offset: number, noAssert?: boolean): number;
+export declare function writeDoubleBE(buf: Buffer, value: number, offset: number, noAssert?: boolean): number;
+export declare function writeDouble(buf: Buffer, value: number, offset: number, noAssert?: boolean): number;
