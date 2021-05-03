69705a7c9ff7e9208bd2869343d89d588ce67afc
Yonggang Luo <luoyonggang@gmail.com>
2021-05-04 00:23:24 +0800

    fixe StructType tests

--- test/ffi_tests.cc
+++ test/ffi_tests.cc
@@ -13,6 +13,10 @@ using namespace Napi;
 # define SYMBOL_EXPORT __attribute__((visibility("default")))
 #endif
 
+#ifdef _WIN32
+  #define __alignof__ __alignof
+#endif
+
 /*
  * Exported function with C naming and calling conventions.
  * Used by dynamic_library.js to test symbol lookup.
@@ -275,6 +279,294 @@ Object InitializeArrayTest(Env env, Object exports) {
   return exports;
 }
 
+namespace {
+
+typedef struct _test1 {
+  int a;
+  int b;
+  double c;
+} test1;
+
+typedef struct _test2 {
+  int a;
+  double b;
+  int c;
+} test2;
+
+typedef struct _test3 {
+  double a;
+  int b;
+  int c;
+} test3;
+
+typedef struct _test4 {
+  double a;
+  double b;
+  int c;
+} test4;
+
+typedef struct _test5 {
+  int a;
+  double b;
+  double c;
+} test5;
+
+typedef struct _test6 {
+  char a;
+  short b;
+  int c;
+} test6;
+
+typedef struct _test7 {
+  int a;
+  short b;
+  char c;
+} test7;
+
+typedef struct _test8 {
+  int a;
+  short b;
+  char c;
+  char d;
+} test8;
+
+typedef struct _test9 {
+  int a;
+  short b;
+  char c;
+  char d;
+  char e;
+} test9;
+
+typedef struct _test10 {
+  test1 a;
+  char b;
+} test10;
+
+// this one simulates the `ffi_type` struct
+typedef struct _test11 {
+  size_t a;
+  unsigned short b;
+  unsigned short c;
+  struct _test11 **d;
+} test11;
+
+typedef struct _test12 {
+  char *a;
+  int b;
+} test12;
+
+typedef struct _test13 {
+  char a;
+  char b[2];
+} test13;
+
+typedef struct _test14 {
+  char a;
+  char b[2];
+  short c;
+  char d;
+} test14;
+
+typedef struct _test15 {
+  test1 a;
+  test1 b;
+} test15;
+
+typedef struct _test16 {
+  double a[10];
+  char b[3];
+  int c[6];
+} test16;
+
+typedef struct _test17 {
+  char a[3];
+} test17;
+
+typedef struct _test18 {
+  test17 a[100];
+} test18;
+
+/* test19 example is from libdespotify
+ * See: https://github.com/TooTallNate/ref-struct/issues/1
+ */
+
+#define STRING_LENGTH 256
+typedef struct _test19 {
+  bool has_meta_data;
+  bool playable;
+  bool geo_restricted;
+  unsigned char track_id[33];
+  unsigned char file_id[41];
+  unsigned int file_bitrate;
+  unsigned char album_id[33];
+  unsigned char cover_id[41];
+  unsigned char *key;
+
+  char *allowed;
+  char *forbidden;
+
+  char title[STRING_LENGTH];
+  struct artist* artist;
+  char album[STRING_LENGTH];
+  int length;
+  int tracknumber;
+  int year;
+  float popularity;
+  struct _test19 *next; /* in case of multiple tracks
+                          in an album or playlist struct */
+} test19;
+
+#pragma pack(1)
+typedef struct _test20 {
+  char a;
+  void *p;
+} test20;
+
+#pragma pack()
+typedef struct _test21 {
+  char a;
+  void *p;
+} test21;
+
+Object InitializeStructTest(Env env, Object exports) {
+  exports["test1 sizeof"] = (sizeof(test1));
+  exports["test1 alignof"] = (__alignof__(test1));
+  exports["test1 offsetof a"] = (offsetof(test1, a));
+  exports["test1 offsetof b"] = (offsetof(test1, b));
+  exports["test1 offsetof c"] = (offsetof(test1, c));
+
+  exports["test2 sizeof"] = (sizeof(test2));
+  exports["test2 alignof"] = (__alignof__(test2));
+  exports["test2 offsetof a"] = (offsetof(test2, a));
+  exports["test2 offsetof b"] = (offsetof(test2, b));
+  exports["test2 offsetof c"] = (offsetof(test2, c));
+
+  exports["test3 sizeof"] = (sizeof(test3));
+  exports["test3 alignof"] = (__alignof__(test3));
+  exports["test3 offsetof a"] = (offsetof(test3, a));
+  exports["test3 offsetof b"] = (offsetof(test3, b));
+  exports["test3 offsetof c"] = (offsetof(test3, c));
+
+  exports["test4 sizeof"] = (sizeof(test4));
+  exports["test4 alignof"] = (__alignof__(test4));
+  exports["test4 offsetof a"] = (offsetof(test4, a));
+  exports["test4 offsetof b"] = (offsetof(test4, b));
+  exports["test4 offsetof c"] = (offsetof(test4, c));
+
+  exports["test5 sizeof"] = (sizeof(test5));
+  exports["test5 alignof"] = (__alignof__(test5));
+  exports["test5 offsetof a"] = (offsetof(test5, a));
+  exports["test5 offsetof b"] = (offsetof(test5, b));
+  exports["test5 offsetof c"] = (offsetof(test5, c));
+
+  exports["test6 sizeof"] = (sizeof(test6));
+  exports["test6 alignof"] = (__alignof__(test6));
+  exports["test6 offsetof a"] = (offsetof(test6, a));
+  exports["test6 offsetof b"] = (offsetof(test6, b));
+  exports["test6 offsetof c"] = (offsetof(test6, c));
+
+  exports["test7 sizeof"] = (sizeof(test7));
+  exports["test7 alignof"] = (__alignof__(test7));
+  exports["test7 offsetof a"] = (offsetof(test7, a));
+  exports["test7 offsetof b"] = (offsetof(test7, b));
+  exports["test7 offsetof c"] = (offsetof(test7, c));
+
+  exports["test8 sizeof"] = (sizeof(test8));
+  exports["test8 alignof"] = (__alignof__(test8));
+  exports["test8 offsetof a"] = (offsetof(test8, a));
+  exports["test8 offsetof b"] = (offsetof(test8, b));
+  exports["test8 offsetof c"] = (offsetof(test8, c));
+  exports["test8 offsetof d"] = (offsetof(test8, d));
+
+  exports["test9 sizeof"] = (sizeof(test9));
+  exports["test9 alignof"] = (__alignof__(test9));
+  exports["test9 offsetof a"] = (offsetof(test9, a));
+  exports["test9 offsetof b"] = (offsetof(test9, b));
+  exports["test9 offsetof c"] = (offsetof(test9, c));
+  exports["test9 offsetof d"] = (offsetof(test9, d));
+  exports["test9 offsetof e"] = (offsetof(test9, e));
+
+  exports["test10 sizeof"] = (sizeof(test10));
+  exports["test10 alignof"] = (__alignof__(test10));
+  exports["test10 offsetof a"] = (offsetof(test10, a));
+  exports["test10 offsetof b"] = (offsetof(test10, b));
+
+  exports["test11 sizeof"] = (sizeof(test11));
+  exports["test11 alignof"] = (__alignof__(test11));
+  exports["test11 offsetof a"] = (offsetof(test11, a));
+  exports["test11 offsetof b"] = (offsetof(test11, b));
+  exports["test11 offsetof c"] = (offsetof(test11, c));
+  exports["test11 offsetof d"] = (offsetof(test11, d));
+
+  exports["test12 sizeof"] = (sizeof(test12));
+  exports["test12 alignof"] = (__alignof__(test12));
+  exports["test12 offsetof a"] = (offsetof(test12, a));
+  exports["test12 offsetof b"] = (offsetof(test12, b));
+
+  exports["test13 sizeof"] = (sizeof(test13));
+  exports["test13 alignof"] = (__alignof__(test13));
+  exports["test13 offsetof a"] = (offsetof(test13, a));
+  exports["test13 offsetof b"] = (offsetof(test13, b));
+
+  exports["test14 sizeof"] = (sizeof(test14));
+  exports["test14 alignof"] = (__alignof__(test14));
+  exports["test14 offsetof a"] = (offsetof(test14, a));
+  exports["test14 offsetof b"] = (offsetof(test14, b));
+  exports["test14 offsetof c"] = (offsetof(test14, c));
+  exports["test14 offsetof d"] = (offsetof(test14, d));
+
+  exports["test15 sizeof"] = (sizeof(test15));
+  exports["test15 alignof"] = (__alignof__(test15));
+  exports["test15 offsetof a"] = (offsetof(test15, a));
+  exports["test15 offsetof b"] = (offsetof(test15, b));
+
+  exports["test16 sizeof"] = (sizeof(test16));
+  exports["test16 alignof"] = (__alignof__(test16));
+  exports["test16 offsetof a"] = (offsetof(test16, a));
+  exports["test16 offsetof b"] = (offsetof(test16, b));
+  exports["test16 offsetof c"] = (offsetof(test16, c));
+
+  exports["test17 sizeof"] = (sizeof(test17));
+  exports["test17 alignof"] = (__alignof__(test17));
+  exports["test17 offsetof a"] = (offsetof(test17, a));
+
+  exports["test18 sizeof"] = (sizeof(test18));
+  exports["test18 alignof"] = (__alignof__(test18));
+  exports["test18 offsetof a"] = (offsetof(test18, a));
+
+  exports["test19 sizeof"] = (sizeof(test19));
+  exports["test19 alignof"] = (__alignof__(test19));
+  exports["test19 offsetof has_meta_data"] = (offsetof(test19, has_meta_data));
+  exports["test19 offsetof playable"] = (offsetof(test19, playable));
+  exports["test19 offsetof geo_restricted"] = (offsetof(test19, geo_restricted));
+  exports["test19 offsetof track_id"] = (offsetof(test19, track_id));
+  exports["test19 offsetof file_id"] = (offsetof(test19, file_id));
+  exports["test19 offsetof file_bitrate"] = (offsetof(test19, file_bitrate));
+  exports["test19 offsetof album_id"] = (offsetof(test19, album_id));
+  exports["test19 offsetof cover_id"] = (offsetof(test19, cover_id));
+  exports["test19 offsetof key"] = (offsetof(test19, key));
+  exports["test19 offsetof allowed"] = (offsetof(test19, allowed));
+  exports["test19 offsetof forbidden"] = (offsetof(test19, forbidden));
+  exports["test19 offsetof title"] = (offsetof(test19, title));
+  exports["test19 offsetof artist"] = (offsetof(test19, artist));
+  exports["test19 offsetof album"] = (offsetof(test19, album));
+  exports["test19 offsetof length"] = (offsetof(test19, length));
+  exports["test19 offsetof tracknumber"] = (offsetof(test19, tracknumber));
+  exports["test19 offsetof year"] = (offsetof(test19, year));
+  exports["test19 offsetof popularity"] = (offsetof(test19, popularity));
+  exports["test19 offsetof next"] = (offsetof(test19, next));
+
+  exports["test20 sizeof"] = (sizeof(test20));
+  exports["test20 alignof"] = (__alignof__(test20));
+
+  exports["test21 sizeof"] = (sizeof(test21));
+  exports["test21 alignof"] = (__alignof__(test21));
+  return exports;
+}
+
+} // anonymous namespace
+
 Object Initialize(Env env, Object exports) {
 #if WIN32
   // initialize "floating point support" on Windows?!?!
@@ -311,6 +603,9 @@ Object Initialize(Env env, Object exports) {
 
   Napi::Object arrayTest = Napi::Object::New(env);
   exports["arrayTest"] = InitializeArrayTest(env, arrayTest);
+
+  Napi::Object structTest = Napi::Object::New(env);
+  exports["structTest"] = InitializeStructTest(env, structTest);
 
   return exports;
 }
--- test/struct.js
+++ test/struct.js
@@ -1,25 +1,24 @@
 'use strict';
-var assert = require('assert')
-  , ref = require('ref')
-  , ArrayType = require('ref-array')
-  , Struct = require('../')(ref)
-  , bindings = require('bindings')({ module_root: __dirname, bindings: 'struct_tests' })
+
+const assert = require("assert")
+const { ref, ArrayType, StructType } = require("../")
+const bindings = require('node-gyp-build')(__dirname).structTest;
 
 describe('Struct', function () {
 
   afterEach(gc)
 
   it('should be a function', function () {
-    assert.equal('function', typeof Struct)
+    assert.equal('function', typeof StructType)
   })
 
   it('should return a struct constuctor function', function () {
-    var S = Struct()
+    var S = StructType()
     assert.equal('function', typeof S)
   })
 
   it('should throw when the same field name is speicified more than once', function () {
-    var S = Struct({ a: ref.types.int })
+    var S = StructType({ a: ref.types.int })
     assert.throws(function () {
       S.defineProperty('a', ref.types.int)
     })
@@ -27,12 +26,12 @@ describe('Struct', function () {
 
   it('should throw when given a type with "size" == 0', function () {
     assert.throws(function () {
-      Struct({ v: 'void' })
+      StructType({ v: 'void' })
     })
   })
 
   it('should work in a simple case', function () {
-    var SimpleStruct = Struct({
+    var SimpleStruct = StructType({
         'first': ref.types.byte
       , 'last':  ref.types.byte
     })
@@ -45,7 +44,7 @@ describe('Struct', function () {
   })
 
   it('should work in a more complex case', function () {
-    var MegaStruct = Struct({
+    var MegaStruct = StructType({
         'byteVal': ref.types.byte
       , 'int8Val': ref.types.int8
       , 'int16Val': ref.types.int16
@@ -56,7 +55,7 @@ describe('Struct', function () {
       , 'doubleVal': ref.types.double
       , 'pointer': ref.refType('void')
     })
-    var msTestPtr = new Buffer(1)
+    var msTestPtr = Buffer.alloc(1)
     var ms = new MegaStruct({
         byteVal: 100
       , int8Val: -100
@@ -81,11 +80,11 @@ describe('Struct', function () {
 
   it('should allow Struct nesting', function () {
 
-    var ChildStruct = Struct({
+    var ChildStruct = StructType({
         'a': ref.types.int
       , 'b': ref.types.int
     })
-    var ParentStruct = Struct({
+    var ParentStruct = StructType({
         'childA': ChildStruct
       , 'childB': ChildStruct
     })
@@ -104,7 +103,7 @@ describe('Struct', function () {
   describe('string type identifiers', function () {
 
     it('should work with string type identifiers', function () {
-      var S = Struct({
+      var S = StructType({
           'int': 'int'
         , 'long': 'long'
         , 'string': 'string'
@@ -116,7 +115,7 @@ describe('Struct', function () {
     })
 
     it('should work as expected with "void *" as a type', function () {
-      var S = Struct({
+      var S = StructType({
         'ptr1': 'void *',
         'ptr2': 'void *'
       })
@@ -133,7 +132,7 @@ describe('Struct', function () {
   describe('set()', function () {
 
     it('should work to set() an Object', function () {
-      var Foo = Struct({
+      var Foo = StructType({
         test1: ref.types.int,
         test2: ref.types.int
       });
@@ -161,7 +160,7 @@ describe('Struct', function () {
 
     it('should work to set() a Struct instance', function () {
       // see: https://github.com/TooTallNate/ref-struct/issues/11
-      var Foo = Struct({
+      var Foo = StructType({
         test1: ref.types.int,
         test2: ref.types.int
       });
@@ -192,7 +191,7 @@ describe('Struct', function () {
   describe('ref(), deref()', function () {
 
     it('should work to ref() and then deref() 1 level deep', function () {
-      var S = Struct({ d: 'double' })
+      var S = StructType({ d: 'double' })
       var s = new S({ d: Math.PI })
       var sref = s.ref()
       assert(Buffer.isBuffer(sref))
@@ -228,56 +227,56 @@ describe('Struct', function () {
       })
     }
 
-    var test1 = Struct({
+    var test1 = StructType({
         'a': ref.types.int
       , 'b': ref.types.int
       , 'c': ref.types.double
     })
     test(test1, 1)
 
-    var test2 = Struct({
+    var test2 = StructType({
         'a': ref.types.int
       , 'b': ref.types.double
       , 'c': ref.types.int
     })
     test(test2, 2)
 
-    var test3 = Struct({
+    var test3 = StructType({
         'a': ref.types.double
       , 'b': ref.types.int
       , 'c': ref.types.int
     })
     test(test3, 3)
 
-    var test4 = Struct({
+    var test4 = StructType({
         'a': ref.types.double
       , 'b': ref.types.double
       , 'c': ref.types.int
     })
     test(test4, 4)
 
-    var test5 = Struct({
+    var test5 = StructType({
         'a': ref.types.int
       , 'b': ref.types.double
       , 'c': ref.types.double
     })
     test(test5, 5)
 
-    var test6 = Struct({
+    var test6 = StructType({
         'a': ref.types.char
       , 'b': ref.types.short
       , 'c': ref.types.int
     })
     test(test6, 6)
 
-    var test7 = Struct({
+    var test7 = StructType({
         'a': ref.types.int
       , 'b': ref.types.short
       , 'c': ref.types.char
     })
     test(test7, 7)
 
-    var test8 = Struct({
+    var test8 = StructType({
         'a': ref.types.int
       , 'b': ref.types.short
       , 'c': ref.types.char
@@ -285,7 +284,7 @@ describe('Struct', function () {
     })
     test(test8, 8)
 
-    var test9 = Struct({
+    var test9 = StructType({
         'a': ref.types.int
       , 'b': ref.types.short
       , 'c': ref.types.char
@@ -294,13 +293,13 @@ describe('Struct', function () {
     })
     test(test9, 9)
 
-    var test10 = Struct({
+    var test10 = StructType({
         'a': test1
       , 'b': ref.types.char
     })
     test(test10, 10)
 
-    var test11 = Struct()
+    var test11 = StructType()
     test11.defineProperty('a', ref.types.size_t)
     test11.defineProperty('b', ref.types.ushort)
     test11.defineProperty('c', ref.types.ushort)
@@ -309,19 +308,19 @@ describe('Struct', function () {
     test11.defineProperty('d', test11_ptr_ptr)
     test(test11, 11)
 
-    var test12 = Struct({
+    var test12 = StructType({
         'a': ref.refType(ref.types.char)
       , 'b': ref.types.int
     })
     test(test12, 12)
 
-    var test13 = Struct({
+    var test13 = StructType({
         'a': ref.types.char
       , 'b': ArrayType('char', 2)
     })
     test(test13, 13)
 
-    var test14 = Struct({
+    var test14 = StructType({
         'a': ref.types.char
       , 'b': ArrayType('char', 2)
       , 'c': ref.types.short
@@ -329,25 +328,25 @@ describe('Struct', function () {
     })
     test(test14, 14)
 
-    var test15 = Struct({
+    var test15 = StructType({
         'a': test1
       , 'b': test1
     })
     test(test15, 15)
 
-    var test16 = Struct({
+    var test16 = StructType({
         'a': ArrayType('double', 10)
       , 'b': ArrayType('char', 3)
       , 'c': ArrayType('int', 6)
     })
     test(test16, 16)
 
-    var test17 = Struct({
+    var test17 = StructType({
         'a': ArrayType('char', 3)
     })
     test(test17, 17)
 
-    var test18 = Struct({
+    var test18 = StructType({
         'a': ArrayType(test17, 100)
     })
     test(test18, 18)
@@ -355,7 +354,7 @@ describe('Struct', function () {
     // this example from libdespotify
     // see: https://github.com/TooTallNate/ref-struct/issues/1
     var STRING_LENGTH = 256;
-    var test19 = Struct();
+    var test19 = StructType();
     test19.defineProperty('has_meta_data', 'bool');
     test19.defineProperty('playable', 'bool');
     test19.defineProperty('geo_restricted', 'bool');
@@ -382,11 +381,11 @@ describe('Struct', function () {
   describe('packed struct', function () {
 
     it('with-padding/no-padding struct', function () {
-      var np = Struct({ a: 'char', p: ref.refType('void') }, {packed: true})
+      var np = StructType({ a: 'char', p: ref.refType('void') }, {packed: true})
       assert.equal(bindings['test20 sizeof'], np.size)
       assert.equal(bindings['test20 alignof'], np.alignment)
 
-      var wp = Struct({ a: 'char', p: ref.refType('void') })
+      var wp = StructType({ a: 'char', p: ref.refType('void') })
       assert.equal(bindings['test21 sizeof'], wp.size)
       assert.equal(bindings['test21 alignof'], wp.alignment)
     })
